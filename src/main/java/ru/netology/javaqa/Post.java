41c321dfb5852c8c2695b955470ce225d3d78036
Roman P <r.i.polyakov@yandex.ru>
2023-02-04 17:11:14 +0300

    first commit

---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.javaqa;
+
+public class Post {
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    boolean subscription;
+    public FormDate birthday = new FormDate();
+}
